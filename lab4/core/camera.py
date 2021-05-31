dedab052e9590afa7af6f84e45f016246c648323
VKuzia <v.kuzia.mail@gmail.com>
2021-06-01 01:53:39 +0300

    Fix lightning bug

--- lab4/core/camera.py
+++ lab4/core/camera.py
@@ -6,7 +6,7 @@ class MovingCamera(KeyboardCamera):
     def __init__(self, keys):
         super().__init__(keys, fov=60.0, near=1, far=10)
         self.keys = keys
-        self.velocity = 1.0
-        self.mouse_sensitivity = 0.1
+        self.velocity = 2.0
+        self.mouse_sensitivity = 0.17
         self.set_position(0, 0, 5)
 
--- lab4/core/resources/shaders.glsl
+++ lab4/core/resources/shaders.glsl
@@ -9,6 +9,9 @@ in vec2 in_texcoord_0;
 uniform mat4 model;
 uniform mat4 view;
 uniform mat4 proj;
+uniform vec3 global_light_pos;
+
+out vec3 light_pos;
 
 out vec2 uv;
 out vec3 pos;
@@ -17,24 +20,26 @@ out vec3 normal;
 void main() {
     mat4 view_model = view * model;
     gl_Position = proj * view_model * vec4(in_position, 1.0);
-    pos = gl_Position.xyz;
+    pos = vec3(view_model * vec4(in_position, 1.0));
     uv = in_texcoord_0;
     mat3 view_normal = inverse(transpose(mat3(view_model)));
-    normal = view_normal * normalize(in_normal);
+    normal  = mat3(transpose(inverse(view_model))) * in_normal;
+    light_pos = vec3(view * vec4(global_light_pos, 1.0));
 }
 
 #elif defined FRAGMENT_SHADER
 
-uniform sampler2D image;
 in vec2 uv;
 in vec3 normal;
 in vec3 pos;
+in vec3 light_pos;
+
+uniform sampler2D image;
+uniform vec3 light_color;
 
 out vec4 out_color;
 
 void main() {
-    vec3 light_color = vec3(1.0, 1.0, 1.0);
-    vec3 light_pos = vec3(10.0, 10.0, 10.0);
     vec3 ambient = 0.2 * light_color;
     vec3 norm = normalize(normal);
     vec3 light_dir = normalize(light_pos - pos);
